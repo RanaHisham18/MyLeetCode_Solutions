f16cffb106a027a4882f1e0eeddb53b252a9e66d
Rana Hisham <81534123+RanaHisham18@users.noreply.github.com>
2022-10-30 11:10:16 +0200

    Time: 432 ms (71.63%), Space: 45.1 MB (57.25%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.kt
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
-        var x = if (nums.size > 0) 1 else 0
+        var x = if (nums.size > 0) {1} else{0} 
 
         for (i in 1 until nums.size) {
             if (nums[i] == nums[i-1])
