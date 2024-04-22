04a4c411349d0ab2eb4bfbed79f54564c848db51
Rana Hisham <81534123+RanaHisham18@users.noreply.github.com>
2024-04-22 22:11:41 +0200

    Time: 0 ms (100.00%) | Memory: 44.4 MB (68.82%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for (int i = 0 ; i< image.length ; i++){
+            int low = 0 ;
+            int high = image[i].length - 1;
+
+            while (low <= high){
+                if (image[i][low] == image[i][high]){
+                    image[i][low] = 1 - image[i][low];
+                    image[i][high] = image[i][low];
+                }
+            
+
+            low ++;
+            high --; }
+        }  return image; 
+    } 
+}
